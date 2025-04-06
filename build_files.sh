eae0d4e670442e71f1510972d976c620b5e56b44
Santanu064 <santanughosh.keunda@gmail.com>
2025-04-06 19:52:32 +0530

    no

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,4 @@ echo "BUILD START"
 python3.9 -m pip install --upgrade pip
 python3.9 -m pip install --upgrade -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
-mkdir -p staticfiles_build
-# Move collected static files to output directory
-cp -r staticfiles/* staticfiles_build/
 echo "BUILD END"
