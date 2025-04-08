6a75867630f62a05d57c7ff68d348c7528162f74
santanu ghosh <131254926+Santanu064@users.noreply.github.com>
2025-04-08 17:41:47 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo "Build start"
 python3.9 -m pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt
+python3.9 pip freeze > requirements.txt
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 echo "Build end"
