7408471c13ee8c13f5ef8d9e3336eae36c5f0de8
Santanu064 <santanughosh.keunda@gmail.com>
2025-04-08 17:08:53 +0530

    new update

--- build_files.sh
+++ build_files.sh
@@ -3,4 +3,5 @@ python3.9 -m pip install -r requirements.txt
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 python3.9 manage.py collectstatic --noinput
+rm -rf staticfiles_build
 echo "Build end"
