e2eac7924843bdc92698a4eda1c44155376c328b
Santanu064 <santanughosh.keunda@gmail.com>
2025-04-08 17:10:58 +0530

    new update

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,4 @@ echo "Build start"
 python3.9 -m pip install -r requirements.txt
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
-python3.9 manage.py collectstatic --noinput
-rm -rf staticfiles_build
 echo "Build end"
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -136,7 +136,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[ os.path.join(BASE_DIR,'static') ]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles' ,'static')
+STATIC_ROOT = os.path.join(BASE_DIR ,'static')
 
 
 # Default primary key field type
